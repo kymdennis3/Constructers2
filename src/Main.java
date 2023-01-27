1b795b0dd6a5aec3227e0b7aad94ac8e1172cde3
Kimtai <kymdennis3@gmail.com>
2023-01-27 06:50:36 +0300

    created a second object(human1) and instantiated the name

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         Human human = new Human("Rick", 65, 70.0);
+        Human human1 = new Human("Morty", 16, 50);
         //System.out.println(human.age);
-        System.out.println(human.name);
+        //System.out.println(human.name);
+        //System.out.println(human.weight);
+
+        System.out.println(human1.name);
     }
 }
