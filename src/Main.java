75fd9f37cc96196083c4c06fdb0e2c8f96b3198c
Kimtai <kymdennis3@gmail.com>
2023-02-03 22:25:24 +0300

    created a function drink

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         Human human = new Human("Rick", 65, 70.0);
         Human human1 = new Human("Morty", 16, 50);
-        //System.out.println(human.age);
+        System.out.println(human.age);
         //System.out.println(human.name);
         //System.out.println(human.weight);
 
